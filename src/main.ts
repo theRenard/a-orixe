14153b2df12eebced59867b5733d7034f1d5441f
Daniele Tabanella <daniele.tabanella@gmail.com>
2026-02-10 23:28:41 +0100

    feat: add styles.css import and clean up design-tokens.css
    - Imported styles.css in main.ts for additional styling support.
    - Removed the import statement for styles.css from design-tokens.css to streamline the file.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 import { i18n } from './i18n'
 import './styles/reset.css'
 import './styles/design-tokens.css'
+import './styles/vars/styles.css'
 
 const app = createApp(App)
 
